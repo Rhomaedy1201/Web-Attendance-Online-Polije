89b319ee5beb3f30e075fabad5a00ca6630fb171
Muhammad Rhomaedi <rhomaedycreator@gmail.com>
2025-01-23 23:18:15 +0700

    first commit

---
+++ resources/views/partials/sidebar.blade.php
@@ -0,0 +1,26 @@
+<!-- Sidebar -->
+<div class="sidebar sidebar-style-2">			
+    <div class="sidebar-wrapper scrollbar scrollbar-inner">
+        <div class="sidebar-content">
+            <ul class="nav nav-primary">
+                <li class="nav-item active">
+                    <a data-toggle="collapse" href="#dashboard" class="collapsed" aria-expanded="false">
+                        <i class="fas fa-home"></i>
+                        <p>Dashboard</p>
+                        <span class="caret"></span>
+                    </a>
+                    <div class="collapse" id="dashboard">
+                        <ul class="nav nav-collapse">
+                            <li>
+                                <a href="../demo1/index.html">
+                                    <span class="sub-item">Dashboard 1</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
+<!-- End Sidebar -->
