f3510be79aaa0721595a646c7c6aa3cf39f9a10b
Muhammad Rhomaedi <rhomaedycreator@gmail.com>
2025-01-24 12:39:47 +0700

    main: add partisals and layouts

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="footer">
+    <div class="container-fluid">
+        <div class="copyright ml-auto">
+            &copy; {{ date('Y') }} MyApp. All rights reserved.
+        </div>				
+    </div>
+</footer>
