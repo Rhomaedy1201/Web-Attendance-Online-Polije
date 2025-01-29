43d42473141394e20825ab69df170f80d7e6dc76
Muhammad Rhomaedi <rhomaedycreator@gmail.com>
2025-01-29 20:56:28 +0700

    main: fix login

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -3,7 +3,7 @@
     <div class="logo-header" data-background-color="blue">
         
         <a href="index.html" class="logo">
-            <img src="{{ asset('template/assets/img/logo.svg') }}" alt="navbar brand" class="navbar-brand">
+            <img src="{{ asset('template/assets/img/logo/polije.png') }}" alt="navbar brand" class="navbar-brand" style="width: 70px">
         </a>
         <button class="navbar-toggler sidenav-toggler ml-auto" type="button" data-toggle="collapse" data-target="collapse" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon">
