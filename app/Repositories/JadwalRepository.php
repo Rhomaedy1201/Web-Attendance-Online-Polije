530e5059a7f66030b8cf82a6fc7e0c666f20abd9
Muhammad Rhomaedi <rhomaedycreator@gmail.com>
2025-02-02 12:06:34 +0700

    main: add repositories all

---
+++ app/Repositories/JadwalRepository.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Repositories;
+
+use JasonGuru\LaravelMakeRepository\Repository\BaseRepository;
+//use Your Model
+
+/**
+ * Class JadwalRepository.
+ */
+class JadwalRepository extends BaseRepository
+{
+    /**
+     * @return string
+     *  Return the model
+     */
+    public function model()
+    {
+        //return YourModel::class;
+    }
+}
